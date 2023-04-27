55c86bd36eb008b687be88662d4e826a894aa5d3
Colter Snyder <programmer114@gmail.com>
2023-04-27 13:11:45 -0600

    C Sharp Randoms Created
    C Sharp Randoms Created

--- randoms/randoms.cs
+++ randoms/randoms.cs
@@ -1,17 +1,16 @@
 using System;
-using System.Random;
 using System.IO;
 
 namespace RandomTest {
     class Randoms {
-        public int getRandom(){
+        public static int getRandom(){
             var rand = new Random();
-            return rand.NextInt();
+            return rand.Next();
         }
 
         static void Main(String[] args){
             for(int i = 0; i < 50; i++) {
-                File.AppendAllText("c_sharp_random.txt", getRandom.ToString());
+                File.AppendAllText("c_sharp_random.txt", getRandom().ToString());
             }
         }
     }
