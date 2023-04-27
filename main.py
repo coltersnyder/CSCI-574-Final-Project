a2b393b14a177c365e1374bbd81626501093795e
Colter Snyder <programmer114@gmail.com>
2023-04-27 11:44:37 -0600

    R Number Analysed
    The Random Number Generated by R is Analyzed

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import os
 
 if __name__ == "__main__":
     print(os.getcwd())
+    rNum = ''
+    with open('randoms/Rnumbers.txt', 'r') as f:
+        rNum = f.read()
+    rNum = int(rNum)
+    rNum = str(bin(rNum))
+    print(rNum[2:100])
