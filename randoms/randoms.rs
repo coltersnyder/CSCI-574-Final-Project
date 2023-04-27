da9e488f80ac58aca39ace1457a694fe1c140650
bgranmoeMines <90219633+bgranmoeMines@users.noreply.github.com>
2023-04-27 17:28:29 -0600

    Update randoms.rs
    Rust is stupid btw (would not recommend) 0/5 stars

--- randoms/randoms.rs
+++ randoms/randoms.rs
@@ -1,8 +1,12 @@
 use rand::Rng;
 
-fn main(){
+fn main() {
     let mut rng = rand::thread_rng();
-
-    let n: u16 = rng.gen()
-    println!("Random u16: {}", n);
+    let random_numbers: Vec<u32> = (0..50).map(|_| rng.gen_range(1..=100)).collect();
+    
+    let mut list_of_numbers = String::new();
+    for num in random_numbers {
+        list_of_numbers.push_str(&num.to_string());
+    }
+    println!("{}", list_of_numbers);
 }
